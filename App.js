c70805fb1a0264ee571b65a477c3a290826aaea9
NurZan111 <55637437+NurZan111@users.noreply.github.com>
2022-12-12 21:17:29 +0600

    anecdotes
    1.12*: anecdotes step3

--- App.js
+++ App.js
@@ -1,48 +1,45 @@
 import { useState } from 'react'
 
-const Statistics =(props)=>{
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ] 
+  const [points, setPoints] = useState({ point:new Array(7).fill(0), maxPoints: 0, setMaxSelected: ' '});
+  const [selected, setSelected] = useState(0);
 
-  let all = props.good + props.neutral + props.bad
-  let average = (props.good - props.bad)/all
-  let positive = (props.good/all)*100 
-
-  if(props.good === 0 && props.bad === 0 &&props.neutral === 0){
-    return(<div>
-      <p>No feedback given</p>
-    </div>)
+  const randomNum = () => {
+    const num = Math.floor(Math.random()*anecdotes.length)
+    setSelected(num)
   }
 
-return(<table>
-      <StatisticsLine text='good' value={props.good}/>
-      <StatisticsLine text='netral' value={props.neutral}/>
-      <StatisticsLine text='bad' value={props.bad}/>
-      <StatisticsLine text='all' value={all}/>
-      <StatisticsLine text='average' value={average}/>
-      <StatisticsLine text='positive' value={positive}/>
-  </table>)
-}
-const StatisticsLine = (props)=>{
-  return(<tr><th>{props.text}</th> <td>{props.value}</td></tr>)
-}
-const Button = (props)=>{
-  return(<button onClick={props.onClick}>{props.value}</button>)
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
-  
+  const addVote = () => {
+    const newPoints = points.point;
+    newPoints[selected] += 1
+    const indexMax = newPoints.indexOf(Math.max(...newPoints))
+    const newArray = {
+      point: [...newPoints],
+      maxPoints: newPoints[indexMax],
+      setMaxSelected: anecdotes[indexMax]
+    }
+    setPoints(newArray)
+  }
+console.log(points.maxPoints)
+ console.log(points.setMaxSelected)
   return (
     <div>
-      <h3>Give feedback</h3>
-      <Button onClick={()=> setGood(good +1)} value='good'/>
-      <Button onClick={()=> setNeutral(neutral +1)} value='neutral'/>
-      <Button onClick={()=> setBad(bad +1)} value='bad'/>
-      <h3>Statistics</h3>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <p>{anecdotes[selected]}</p> 
+      <p>has {points.point[selected]} votes</p>
+      <button onClick={()=> addVote()}>vote</button>
+      <button onClick={() => randomNum()}>next anecdotes</button>
+      <p>Anecdotes with most votes</p>
+      <p>{points.setMaxSelected}</p>
+      <p>has {points.maxPoints} vote</p>
     </div>
   )
 }
